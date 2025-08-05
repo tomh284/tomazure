c56a6d422e681f48ae7520cb67e0b2478fc39c01
tomh284 <thomasholds@hotmail.co.uk>
2025-08-05 16:20:43 +0100

    Update main.tf

--- main.tf
+++ main.tf
@@ -12,6 +12,14 @@ provider "azurerm" {
   subscription_id = "d1680569-4815-4e74-845b-22b4dce0da01"
 }
 
+terraform {
+  backend "azurerm" {
+    resource_group_name  = "RG1"
+    storage_account_name = "jenkinsstatetomh"
+    container_name       = "terraform-state"
+    key                  = "terraform.tfstate"
+  }
+}
 
 resource "azurerm_resource_group" "example" {
   name     = "RG2"
